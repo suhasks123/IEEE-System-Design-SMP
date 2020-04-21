2664a7df39383d5689458fc4c733207d1e011287
dharmicksai <55967538+dharmicksai@users.noreply.github.com>
2020-04-21 14:40:56 +0530

    Added Comments

--- Assignment-1-Socket-Programming/Dharmick/echo-server.py
+++ Assignment-1-Socket-Programming/Dharmick/echo-server.py
@@ -2,15 +2,15 @@ import socket
 Host = '127.0.0.1'
 Port = 65432
 
-with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    s.bind((Host,Port))
-    s.listen()
-    conn, addr = s.accept()
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s: #creating socket
+    s.bind((Host,Port)) #binding Socket
+    s.listen() #listening for connection
+    conn, addr = s.accept() #accepting connection
     with conn:
         print("conected by",addr)
         while True:
-            data = conn.recv(1024)
+            data = conn.recv(1024) #reciving data
             if not data:
-                break
-            conn.sendall(data)
+                break #if no data is being recived then clossing connection
+            conn.sendall(data)  #sending back recived data
 
