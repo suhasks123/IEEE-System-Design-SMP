352fe5ce7e94c6eb6846bb7a2eee4544053fa8c5
RAGUL1902 <64137176+RAGUL1902@users.noreply.github.com>
2020-04-22 07:34:52 -0700

    Delete server1.py

--- server1.py
+++
@@ -1,9 +0,0 @@
-import socket
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1024))
-s.listen(5)
-while True :
-    clt, adr=s.accept()
-    print(f'Successfully connected to {adr}  ')
-    clt.send(bytes("This is socket programming", "utf-8"))
-    clt.close()
